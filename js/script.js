084a6399dee0ad519e2f5ed3f7e9eb4658bf49b1
BorisovMisha <bor_mb@mail.ru>
2021-06-29 00:22:45 +0400

    Код JavaScript для главной страницы

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-var search = document.querySelector(".search-btn");
-var form = document.querySelector(".hotel-form");
+const search = document.querySelector(".search-btn");
+const mapImg = document.querySelector(".map-img");
 
-var arriveDate = form.querySelector("[name=arrive-date]");
-var departDate = form.querySelector("[name=depart-date]");
-var adultCount = form.querySelector("[name=adult-count]");
-var childCount = form.querySelector("[name=child-count]");
+if (mapImg)
+  mapImg.classList.add("map-img-close");
 
-var adultMinus = form.querySelector(".minus-btn.adult-btn");
-var adultPlus = form.querySelector(".plus-btn.adult-btn");
-var childMinus = form.querySelector(".minus-btn.child-btn");
-var childPlus = form.querySelector(".plus-btn.child-btn");
+if (search) {
+  const form = document.querySelector(".hotel-form");
+  const arriveDate = form.querySelector("[name=arrive-date]");
+  const departDate = form.querySelector("[name=depart-date]");
+  const adultCount = form.querySelector("[name=adult-count]");
+  const childCount = form.querySelector("[name=child-count]");
+  form.classList.add("hotel-form-close")
+  search.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    form.classList.toggle("hotel-form-open");
+  });
+}
 
 form.addEventListener("submit", function (evt) {
   if (!arriveDate.value || !departDate.value || !adultCount.value || !childCount.value) {
@@ -27,39 +33,5 @@ form.addEventListener("submit", function (evt) {
   } else {
     localStorage.setItem("adultCount", adultCount.value);
     localStorage.setItem("childCount", childCount.value);
-  }
-});
-
-adultMinus.addEventListener("click", function (evt) {
-  if (!adultCount.value) {
-    adultCount.value = 0;
-  } else if (adultCount.value > 0) {
-    adultCount.value = parseInt(adultCount.value) - 1;
-  }
-});
-
-adultPlus.addEventListener("click", function (evt) {
-  if (!adultCount.value) {
-    adultCount.value = 0;
-    adultCount.value = parseInt(adultCount.value) + 1;
-  } else {
-    adultCount.value = parseInt(adultCount.value) + 1;
-  }
-});
-
-childMinus.addEventListener("click", function (evt) {
-  if (!childCount.value) {
-    childCount.value = 0;
-  } else if (childCount.value > 0) {
-    childCount.value = parseInt(childCount.value) - 1;
-  }
-});
-
-childPlus.addEventListener("click", function (evt) {
-  if (!childCount.value) {
-    childCount.value = 0;
-    childCount.value = parseInt(childCount.value) + 1;
-  } else {
-    childCount.value = parseInt(childCount.value) + 1;
   }
 });
