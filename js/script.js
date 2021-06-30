c68526ba29a9f6bcb794da13f3bfb5b34436e4f8
BorisovMisha <bor_mb@mail.ru>
2021-06-30 21:10:11 +0400

    Улучшил пиксель-перфект, исправил замечания

--- js/script.js
+++ js/script.js
@@ -16,23 +16,24 @@ if (search) {
         evt.preventDefault();
         form.classList.toggle("hotel-form-open");
     });
-}
 
-form.addEventListener("submit", function (evt) {
-    if (!arriveDate.value || !departDate.value || !adultCount.value || !childCount.value) {
-        evt.preventDefault();
-        form.classList.add("modal-error");
-        if (!arriveDate.value) {
-            arriveDate.focus();
-        } else if (!departDate.value) {
-            departDate.focus();
-        } else if (!adultCount.value) {
-            adultCount.focus();
-        } else if (!childCount.value) {
-            childCount.focus();
+    form.addEventListener("submit", function (evt) {
+        if (!arriveDate.value || !departDate.value || !adultCount.value || !childCount.value) {
+            evt.preventDefault();
+            form.classList.add("modal-error");
+            if (!arriveDate.value) {
+                arriveDate.focus();
+            } else if (!departDate.value) {
+                departDate.focus();
+            } else if (!adultCount.value) {
+                adultCount.focus();
+            } else if (!childCount.value) {
+                childCount.focus();
+            }
+        } else {
+            localStorage.setItem("adultCount", adultCount.value);
+            localStorage.setItem("childCount", childCount.value);
         }
-    } else {
-        localStorage.setItem("adultCount", adultCount.value);
-        localStorage.setItem("childCount", childCount.value);
-    }
-});
+    });
+
+}
